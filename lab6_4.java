783f7281b47dcd6013e74066dae17dfaaa412789
Varun Sanwaria <59400234+vrn4@users.noreply.github.com>
2020-12-07 18:40:49 +0530

    Update and rename lab8_4.java to lab6_4.java

--- lab6_4.java
+++ lab6_4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class lab8_4 {
+public class lab6_4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
