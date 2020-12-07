68b1bd6e1df4c531756921e69deb170042ced02c
Varun Sanwaria <59400234+vrn4@users.noreply.github.com>
2020-12-07 18:16:23 +0530

    Add files via upload

---
+++ labex1_1.java
@@ -0,0 +1,11 @@
+public class labex1_1 {
+    public static void main(String[] args) {
+        int x = Integer.parseInt(args[0]);
+        if(x%2==0){
+            System.out.println("Even Number");
+        }
+        else{
+            System.out.println("Odd Number");
+        }
+    }
+}
